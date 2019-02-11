d8ba192a088fc366256bd772227db6d9d3a4a428
Gianni <gianni.codevico@gmail.com>
2019-02-11 14:21:54 +0100

    Added Swing Integration

--- src/main/java/com/stecam/piantaggio/piantaggio/PiantaggioApplication.java
+++ src/main/java/com/stecam/piantaggio/piantaggio/PiantaggioApplication.java
@@ -4,23 +4,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 @SpringBootApplication
-public class PiantaggioApplication implements CommandLineRunner {
+public class PiantaggioApplication extends JFrame{
+
+	public PiantaggioApplication() {
+
+		initUI();
+	}
+
+	private void initUI() {
+
+		JButton quitButton = new JButton("Quit");
+
+		quitButton.addActionListener((ActionEvent event) -> {
+			System.exit(0);
+		});
+
+		createLayout(quitButton);
+
+		setTitle("Quit button");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void createLayout(JComponent... arg) {
+
+		Container pane = getContentPane();
+		GroupLayout gl = new GroupLayout(pane);
+		pane.setLayout(gl);
+
+		gl.setAutoCreateContainerGaps(true);
+
+		gl.setHorizontalGroup(gl.createSequentialGroup()
+				.addComponent(arg[0])
+		);
+
+		gl.setVerticalGroup(gl.createSequentialGroup()
+				.addComponent(arg[0])
+		);
+	}
+
 
 	@Autowired
 	UserRepository repository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(PiantaggioApplication.class, args);
+		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(PiantaggioApplication.class)
+				.headless(false).run(args);
+
+		EventQueue.invokeLater(()->{
+			PiantaggioApplication piantaggio = ctx.getBean(PiantaggioApplication.class);
+			piantaggio.setVisible(true);
+
+		});
+		;
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		User userino = new User("Gianni",44);
-		repository.save(userino);
-	}
 }
 
